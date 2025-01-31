d65403c64aa6e03186672fb1dd5a8241d00642a7
ProgrammerLP <developer.programmerlp@gmx.net>
2025-01-31 17:19:36 +0100

    Projektdateien hinzufügen.

---
+++ ZippChat/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ZippChat
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ ZippChat/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ZippChat
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
