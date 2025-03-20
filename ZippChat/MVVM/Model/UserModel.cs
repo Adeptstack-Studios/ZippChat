af0d5199defb9596f316d88638b2d089524099e7
ProgrammerLP <developer.programmerlp@gmx.net>
2025-03-20 18:20:02 +0100

    basic chat app completed

---
+++ ZippChat/MVVM/Model/UserModel.cs
@@ -0,0 +1,8 @@
+﻿namespace ZippChat.MVVM.Model
+{
+    class UserModel
+    {
+        public string Username { get; set; }
+        public string UID { get; set; }
+    }
+}
---
+++ ZippChat/MVVM/View/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ZippChat.MVVM.View
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+
+}
